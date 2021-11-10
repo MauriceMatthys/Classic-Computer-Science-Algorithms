3258fa2577b91b79c85d861dd2d36b37051e5c46
Maurice Matthys <maurice.matthys@student.hogent.be>
2021-11-10 15:01:29 +0100

    Fix Binaire hoop met vaste grootte.py

--- Hoofdstuk 4  - Bomen/Binaire hoop met vaste grootte.py
+++ Hoofdstuk 4  - Bomen/Binaire hoop met vaste grootte.py
@@ -1,12 +1,6 @@
 class BinaryHeap:
 
-
-
-
-
-
-
-    def __init__(self, max_size=10):
+    def __init__(self, max_size=100):
         self.max = max_size
         self.nb = 0
         self.arr = [0]
@@ -17,32 +11,47 @@ class BinaryHeap:
     def get_min_elem(self):
         return self.arr[1]
 
-    def insert_elem(self, item):
+    def insert_elem(self, elem):
         if self.nb == self.max:
             print("binary heap is full")
-        self.nb = self.nb+1
+            return
+        self.nb = self.nb + 1
         index = self.nb
-        self.arr.append(item)
-        while self.arr[index//2] > self.arr[index] and index > 1:
+        self.arr.append(elem)
+        while self.arr[index // 2] > self.arr[index] and index > 1:
             temp = self.arr[index]
-            self.arr[index] = self.arr[index//2]
-            self.arr[index//2] = temp
-            index = index//2
+            self.arr[index] = self.arr[index // 2]
+            self.arr[index // 2] = temp
+            index //= 2
 
     def remove_min_elem(self):
         if self.nb < 1:
-            print("binary heap is empty")
+            print("binary heap is full")
             return
-
         result = self.arr[1]
-        self.nb = self.nb-1
+        self.nb = self.nb - 1
         if self.nb > 0:
             self.arr[1] = self.arr.pop()
-        else: self.arr.pop()
+        else:
+            self.arr.pop()
         index = 1
 
-        while(index*2<= self.,n and self.arr[index] > self.arr[index*2]) or (index*2+1 <= self
+        while index * 2 <= self.nb and self.arr[index] > self.arr[index * 2]:
+            if index * 2 + 1 <= self.nb and self.arr[index * 2 + 1] < self.arr[index * 2]:
+                temp = self.arr[index]
+                self.arr[index] = self.arr[index * 2 + 1]
+                self.arr[index * 2 + 1] = temp
+                index = index * 2 + 1
 
-        if (index *2+1 <= self.nb and self.arr[index*2+1] w self.arr[index*2])
+            else:
+                temp = self.arr[index]
+                self.arr[index] = self.arr[index * 2]
+                self.arr[index * 2] = temp
+                index *= 2
+
+        return result
+
     def __str__(self):
-        pass
+        dummy = self.arr.copy()
+        dummy.pop(0)
+        return str(dummy)
