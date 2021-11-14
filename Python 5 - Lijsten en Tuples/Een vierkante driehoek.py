dc0bb876d315e41990335c2b9101a5182651b0b3
Maurice Matthys <maurice.matthys@student.hogent.be>
2021-11-14 03:21:53 +0100

    Merge vierkante driehoek met oplossing van Mathis

--- Python 5 - Lijsten en Tuples/Een vierkante driehoek.py
+++ Python 5 - Lijsten en Tuples/Een vierkante driehoek.py
@@ -1,22 +1,5 @@
 # import math
-#
-#
-# def combination(n, r):
-#     return int((math.factorial(n)) / ((math.factorial(r)) * math.factorial(n - r)))
-#
-#
-# def driehoek(rows):
-#     if not isinstance(rows, int) or rows < 0:
-#         raise AssertionError("ongeldig aantal rijen")
-#     result = []
-#     for count in range(rows):
-#         row = []
-#         for element in range(count + 1):
-#             row.append(combination(count, element))
-#         result.append(row)
-#     return result
-#
-#
+
 # def zeshoek(row, col):
 #     row -= 1
 #     col -= 1
@@ -36,31 +19,24 @@
 #     return numbers
 #
 #
-# def kwadraat(row, col):
-#     numbers = zeshoek(row, col)
-#
-#     product = math.prod(numbers)
-#     squarert = int(math.sqrt(product))
-#
-#     stringnumbers = [str(i) for i in numbers]
-#
-#     return f'{" x ".join(stringnumbers)} = {math.prod(numbers)} = {squarert} x {squarert}'
 
 import math
 
 
-def driehoek(height):
-    if is_natural_number(height) == False:
+def driehoek(rows):
+    if not is_natural_number(rows):
         raise AssertionError("ongeldig aantal rijen")
-    else:
-        triangle = []
+    result = []
+    for count in range(rows):
         row = []
-        prev_row = []
-        for i in range(0, height + 1):
-            row = [j > 0 and j < i - 1 and i > 2 and prev_row[j - 1] + prev_row[j] or 1 for j in range(0, i)]
-            prev_row = row
-            triangle += [row]
-        return triangle[1:]
+        for element in range(count + 1):
+            row.append(combination(count, element))
+        result.append(row)
+    return result
+
+
+def combination(n, r):
+    return int((math.factorial(n)) / ((math.factorial(r)) * math.factorial(n - r)))
 
 
 def is_natural_number(n):
@@ -80,19 +56,12 @@ def zeshoek(rij, kolom):
     return nieuw
 
 
-def kwadraat(rij, kolom):
-    getallen = zeshoek(rij, kolom)
-    getal1 = getallen[0]
-    getal2 = getallen[1]
-    getal3 = getallen[2]
-    getal4 = getallen[3]
-    getal5 = getallen[4]
-    getal6 = getallen[5]
+def kwadraat(row, col):
+    numbers = zeshoek(row, col)
 
-    product = getal1 * getal2 * getal3 * getal4 * getal5 * getal6
-    wortel = math.sqrt(product)
+    product = math.prod(numbers)
+    squarert = int(math.sqrt(product))
 
-    uitkomst = "%d x %d x %d x %d x %d x %d = %d = %d x %d" % (
-    getal1, getal2, getal3, getal4, getal5, getal6, product, wortel, wortel)
+    stringnumbers = [str(i) for i in numbers]
 
-    return uitkomst
+    return f'{" x ".join(stringnumbers)} = {math.prod(numbers)} = {squarert} x {squarert}'
